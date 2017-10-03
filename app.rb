95e2291881ce265324c2a9566771ce42dad79a32
JpnLavender <irimamekun@gmail.com>
2017-10-04 08:54:32 +0900

    time limit

--- app.rb
+++ app.rb
@@ -1,3 +1,11 @@
 require 'net/http'
 require 'uri'
-Net::HTTP.get_print URI.parse('https://hiyoshi-fes2017.herokuapp.com/send_to_slack')
+require 'time'
+
+now = Time.now
+start = Time.parse("07:00:00")
+finish = Time.parse("21:00:00")
+
+if start < now && finish > now
+  Net::HTTP.get_print URI.parse('https://hiyoshi-fes2017.herokuapp.com/send_to_slack')
+end
